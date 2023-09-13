37d8d3d3f10f60129f5a21f3217b3e73c061de51
slumsk8 <aragaocosta85@gmail.com>
2023-09-12 22:09:05 -0300

    {feat}:Add map marker

--- src/screens/LocationSupermarket/Location.js
+++ src/screens/LocationSupermarket/Location.js
@@ -1,36 +1,37 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { styles } from "./styles";
-import MapView, { Marker } from 'react-native-maps'
+import MapView, { Marker } from "react-native-maps";
 import { useRoute } from "@react-navigation/native";
 
 export default function LocationSupermarket() {
   const route = useRoute();
-  const name = route.params.name
-  const id = route.params.id
-  const lat = route.params.address.lat
-  const lng = route.params.address.lng
+  const name = route.params.name;
+  const id = route.params.id;
+  const lat = route.params.address.lat;
+  const lng = route.params.address.lng;
 
   return (
     <View style={styles.container}>
-      <MapView 
+      <MapView
         style={styles.map}
         key={id}
         initialRegion={{
           latitude: lat,
           longitude: lng,
           latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
+          longitudeDelta: 0.0421,
         }}
-      />
-      <Marker 
-        title={name}
-        pinColor="#ff0000"
-        coordinate={{
-          latitude: lat,
-          longitude: lng
-        }}
-      />
+      >
+        <Marker
+          title={name}
+          pinColor="#ff0000"
+          coordinate={{
+            latitude: lat,
+            longitude: lng,
+          }}
+        />
+      </MapView>
     </View>
   );
 }
